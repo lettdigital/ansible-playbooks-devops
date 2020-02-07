8ee68ca200baccb8470182a2389ce5d1d4073609
Gabriel Chamon Araujo <gchamon@live.com>
2020-02-07 12:10:05 -0300

    adding zabbix sg directly to all instance's network interfaces

--- zabbix-agent/files/add_zabbix_agent_security_group.py
+++ zabbix-agent/files/add_zabbix_agent_security_group.py
@@ -1,5 +1,5 @@
-import sys
 import os
+import sys
 
 import boto3
 
@@ -14,31 +14,32 @@ ec2_resource = boto3.resource("ec2")
 
 
 def main():
-    instance, instance_info = get_instance_class_and_info()
+    instance_info, network_interfaces = get_instance_info_and_network_interfaces()
     vpc_names_by_id = get_vpc_names_by_id()
-    instance_security_group_ids = get_instance_security_group_ids(instance_info)
+    zabbix_agent_sg_ids_by_name = get_zabbix_security_group_ids_by_names()
+    add_zabbix_agent_security_groups_to_all_interfaces(instance_info,
+                                                       network_interfaces,
+                                                       vpc_names_by_id,
+                                                       zabbix_agent_sg_ids_by_name)
+
+
+def add_zabbix_agent_security_groups_to_all_interfaces(instance_info,
+                                                       network_interfaces,
+                                                       vpc_names_by_id,
+                                                       zabbix_agent_sg_ids_by_name):
+    zabbix_agent_sg_id_to_add = (zabbix_agent_sg_ids_by_name[zabbix_agent_security_group_names["production"]]
+                                 if vpc_names_by_id[instance_info["VpcId"]] == production_vpc_name
+                                 else zabbix_agent_sg_ids_by_name[zabbix_agent_security_group_names["development"]])
+
+    for network_interface in network_interfaces:
+        previous_security_groups = [security_group["GroupId"]
+                                    for security_group in network_interface["info"]["Groups"]]
+        network_interface["resource"].modify_attribute(Groups=[*previous_security_groups,
+                                                               zabbix_agent_sg_id_to_add])
+
+
+def get_zabbix_security_group_ids_by_names():
     security_group_pages = ec2_client.get_paginator('describe_security_groups').paginate()
-    security_group_ids_by_name = get_zabbix_security_group_ids_by_names(security_group_pages)
-    security_group_ids_to_add = get_security_ids_to_add_to_instance(instance_info,
-                                                                    instance_security_group_ids,
-                                                                    security_group_ids_by_name, vpc_names_by_id)
-    instance.modify_attribute(Groups=list(security_group_ids_to_add))
-    print("Success")
-
-
-def get_security_ids_to_add_to_instance(instance_info,
-                                        instance_security_group_ids,
-                                        security_group_ids_by_name,
-                                        vpc_names_by_id):
-    zabbix_security_group_to_add = (security_group_ids_by_name[zabbix_agent_security_group_names["production"]]
-                                    if vpc_names_by_id[instance_info["VpcId"]] == production_vpc_name
-                                    else security_group_ids_by_name[zabbix_agent_security_group_names["development"]])
-    security_group_ids_to_add = {*instance_security_group_ids,
-                                 zabbix_security_group_to_add}
-    return security_group_ids_to_add
-
-
-def get_zabbix_security_group_ids_by_names(security_group_pages):
     security_groups = (security_group
                        for page in security_group_pages
                        for security_group in page["SecurityGroups"])
@@ -50,12 +51,6 @@ def get_zabbix_security_group_ids_by_names(security_group_pages):
     return security_group_ids_by_name
 
 
-def get_instance_security_group_ids(instance_info):
-    instance_security_group_ids = {security_group["GroupId"]
-                                   for security_group in instance_info["SecurityGroups"]}
-    return instance_security_group_ids
-
-
 def get_vpc_names_by_id():
     vpc_names_by_id = {vpc["VpcId"]: next(tag["Value"]
                                           for tag in vpc["Tags"]
@@ -64,11 +59,13 @@ def get_vpc_names_by_id():
     return vpc_names_by_id
 
 
-def get_instance_class_and_info():
+def get_instance_info_and_network_interfaces():
     instance_id = sys.argv[1]
     instance_info = ec2_client.describe_instances(InstanceIds=[instance_id])["Reservations"][0]["Instances"][0]
-    instance = ec2_resource.Instance(instance_info["InstanceId"])
-    return instance, instance_info
+    network_interfaces = ({"resource": ec2_resource.NetworkInterface(network_interface["NetworkInterfaceId"]),
+                           "info": network_interface}
+                          for network_interface in instance_info["NetworkInterfaces"])
+    return instance_info, network_interfaces
 
 
 main()
